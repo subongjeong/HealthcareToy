268cdf10d38b8bcd212cbaa8a17568fce63a2c63
Jeong Subong <ifdarlingsun@gmail.com>
2015-12-27 21:04:40 +0900

    스토리보드 구성 후, 첫화면에서 바로 로그인으로 가도록 해두었다.

--- HealthcareToy/ViewController.swift
+++ HealthcareToy/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.hidesBarsOnTap = true
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        self.performSegueWithIdentifier("goto_login", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
